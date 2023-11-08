35c5efa68fb55b2b58975af7100beb71092d2cf9
Ben Williams <ben@719ben.com>
2023-11-08 15:57:13 -0800

    added og info (#17)

--- theme.config.tsx
+++ theme.config.tsx
@@ -3,6 +3,11 @@ import { DocsThemeConfig } from "nextra-theme-docs";
 import Image from "next/image";
 import { useRouter } from "next/router";
 
+const title = "Faraday.dev | Guides";
+const description = "Official guides for Faraday.dev";
+// TODO: add a custom og image
+const targetOGImage = "https://faraday.dev/og-image2.png";
+
 const config: DocsThemeConfig = {
   useNextSeoProps() {
     const { asPath } = useRouter();
@@ -27,10 +32,20 @@ const config: DocsThemeConfig = {
   },
   head: (
     <>
-      <title>Faraday Guides</title>
-      <meta name="description" content="Official guides for Faraday.dev" />
+      <title>Faraday.dev | Guides</title>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="description" content={description} />
+      <meta property="og:url" content="https://docs.faraday.dev" />
+      <meta property="og:title" content={title} />
+      <meta property="og:image" content={targetOGImage} />
+      <meta property="og:description" content={description} />
       <link rel="icon" href="/favicon.png" />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta property="twitter:domain" content="docs.faraday.dev" />
+      <meta property="twitter:url" content="https://docs.faraday.dev" />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      <meta name="twitter:image" content={targetOGImage} />
     </>
   ),
   // this is a hack to hide the edit link
